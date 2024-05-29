7587235eb2c657ddf3423d2d4dbf68d39254bbad
asherdave <sumalpongashkie@gmail.com>
2024-05-30 00:58:01 +0800

    ere

--- src/Components/VerifyFirstPopup.js
+++ src/Components/VerifyFirstPopup.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "../Css/VerifyFirstPopup.css";
 
 export const VerifyFirstPopup = () => {
+    const navigate = useNavigate();
+
+    const handleOkClick = () => {
+      navigate('/userprofile');
+    };
+
   return (
     <div className="verify-first-popup">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <p className="text-wrapper">You need to verify your account first to rent.</p>
-          <div className="group">
-            <div className="overlap-group">
-              <div className="button">OK</div>
-            </div>
+      <div className="overlap-wrappera">
+        <div className="overlaps">
+          <p className="text-wrapperd">You need to verify your account first to rent.</p>
+          <div className="groupq">
+            <button className="overlap-groupt"onClick={handleOkClick}>
+              <div className="div12">OK</div>
+            </button>
           </div>
         </div>
       </div>
