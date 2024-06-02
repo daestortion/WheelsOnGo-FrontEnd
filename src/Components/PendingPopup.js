676d2e823549f30c09087776870ab7f0220dbc23
asherdave <sumalpongashkie@gmail.com>
2024-06-02 22:36:57 +0800

    asd

--- src/Components/PendingPopup.js
+++ src/Components/PendingPopup.js
@@ -4,12 +4,12 @@ import "../Css/PendingPopup.css";
 export const PendingRent = () => {
   return (
     <div className="pending-rent">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <p className="text-wrapper">You already have a pending/active rent.</p>
-          <div className="group">
-            <div className="overlap-group">
-              <div className="div">OK</div>
+      <div className="overlap-wrapper343">
+        <div className="overlap1212">
+          <p className="text-wrapperr3">You already have a pending/active rent.</p>
+          <div className="group121a">
+            <div className="overlap-groupdfas">
+              <div className="div1aa">OK</div>
             </div>
           </div>
         </div>
