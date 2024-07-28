bdf7984412e65006c7bbb3f86f93935c265d3d66
asherdave <sumalpongashkie@gmail.com>
2024-07-28 23:27:08 +0800

    sd

---
+++ src/Components/Report.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../Css/Report.css";
+
+export const Report = () => {
+  return (
+    <div className="report">
+      <div className="overlap-wrapper456">
+        <div className="overlap456">
+          <div className="text-wrapper456">Submit a Report</div>
+          <div className="group456">
+            <div className="overlap-group456">
+              <div className="div456">Submit</div>
+            </div>
+          </div>
+          <div className="close">
+            <img className="vector" alt="Vector" src="vector.svg" />
+          </div>
+          <div className="rectangle" />
+          <div className="text-wrapper-2">Subject:</div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-3">Write here</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
