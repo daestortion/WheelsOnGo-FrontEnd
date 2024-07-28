8252420b8dc31ad4c15dcd0d416240e3dcdf4d4c
asherdave <sumalpongashkie@gmail.com>
2024-07-28 23:32:23 +0800

    asdw

--- src/Components/Report.js
+++ src/Components/Report.js
@@ -8,9 +8,9 @@ export const Report = () => {
         <div className="overlap456">
           <div className="text-wrapper456">Submit a Report</div>
           <div className="group456">
-            <div className="overlap-group456">
+            <button className="overlap-group456">
               <div className="div456">Submit</div>
-            </div>
+            </button>
           </div>
           <div className="close">
             <img className="vector" alt="Vector" src="vector.svg" />
