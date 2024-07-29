408af2238ab305882f96b5cfc7c39b195014ead4
asherdave <sumalpongashkie@gmail.com>
2024-07-29 22:36:56 +0800

    awd

---
+++ src/Components/ReportPopup.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Css/ReportPopup.css";
+
+
+export const ReportSuccess = () => {
+    return (
+        <div className="report-success">
+            <div className="overlap-wrapperjkl">
+                <div className="overlapjkl">
+                    <div className="text-wrapperjkl">Report submitted successfully.</div>
+                    <div className="groupjkl">
+                        <div className="overlap-groupjkl">
+                            <div className="divjkl">OK</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
