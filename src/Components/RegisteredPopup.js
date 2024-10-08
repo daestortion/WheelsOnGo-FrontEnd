89ab0b4486b87efc6c93300c71533b6e06da0012
asherdave <sumalpongashkie@gmail.com>
2024-10-08 22:26:16 +0800

    awd

--- src/Components/RegisteredPopup.js
+++ src/Components/RegisteredPopup.js
@@ -6,23 +6,18 @@ export const RegisteredPopup = () => {
   const navigate = useNavigate();
 
   const handleOkClick = () => {
-    // Close the popup (this could be a state change in the parent component)
-    // Navigate to login page
     navigate('/login');
   };
 
   return (
     <div className="successful-register">
-      <div className="overlap-wrapper">
         <div className="overlap">
-          <div className="ok-text-wrapper">You have successfully registered.</div>
-          <div className="group">
-            <div className="overlap-group23" onClick={handleOkClick}>
-              <div className="ok-div">OK</div>
-            </div>
-          </div>
+          <h1 className="ok-text-wrapper">You have successfully registered.</h1>
+            <button className="overlap-group23" onClick={handleOkClick}>
+              <span className="ok-div">OK</span>
+            </button>
         </div>
-      </div>
+
     </div>
   );
 };
