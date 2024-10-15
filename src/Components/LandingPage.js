026c4279afaf38934b195359aa3049f06598598d
asherdave <sumalpongashkie@gmail.com>
2024-10-15 12:34:25 +0800

    awd

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -14,34 +14,36 @@ export const LandingPage = () => {
         navigate('/cars'); // Navigate to cars page
       };
 
-    return (
+      return (
         <div className="landing-page">
-            <Header />
-            
+          <Header />
+          <div className="content-wrapper">
             <div className="overlap-wrapper31">
-        <div className="overlap-group31">
-          <div className="find-book-and-rent-a31">
-            <span className="text-wrapper31">Find, Book, and Rent a Car in </span>
-            <span className="span31">Easy</span>
-            <span className="text-wrapper31"> Steps.</span>
-            <img className="vector31" alt="Vector" src={homecheck} />
+              <div className="overlap-group31">
+                <div className="find-book-and-rent-a31">
+                  <span className="text-wrapper31">Find, Book, and Rent a Car in </span>
+                  <span className="span31">Easy</span>
+                  <span className="text-wrapper31"> Steps.</span>
+                  <img className="vector31" alt="Vector" src={homecheck} />
+                </div>
+                <span className="div31">Conquer the open road with Wheels On Go</span>
+                <button className="div-wrapper31" onClick={handleCarsClick}>
+                  Book Now
+                </button>
+              </div>
+      
+              <div className="overlap-231">
+                <div className="image-group31">
+                  <img className="rectangle31" alt="Rectangle" src={car1} />
+                  <img className="rectangle-231" alt="Rectangle" src={car3} />
+                  <img className="img31" alt="Rectangle" src={car2} />
+                </div>
+              </div>
+            </div>
           </div>
-          
-          <span className="div31">Conquer the open road with Wheels On Go</span>
-
-          <button className="div-wrapper31" onClick={handleCarsClick}>Book Now</button>
         </div>
-
-        <div className="overlap-231">
-          <div className="image-group31">
-            <img className="rectangle31" alt="Rectangle" src={car1} />
-            <img className="rectangle-231" alt="Rectangle" src={car3} />
-            <img className="img31" alt="Rectangle" src={car2} />
-          </div>
-        </div>
-      </div>
-        </div>
-    );
+      );
+      
 };
 
 export default LandingPage;
